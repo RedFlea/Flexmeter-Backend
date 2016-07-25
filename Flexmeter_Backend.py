2924eb019e78bd45ed84accf199ffcc7aaab3bdf
RedFlea <aleciacio@gmail.com>
2016-07-25 16:32:05 +0200

    WS implementation

--- Flexmeter_Backend.py
+++ Flexmeter_Backend.py
@@ -196,6 +196,177 @@ details = [
     }
 ]
 
+details_ec = [
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.00,
+        "currency": "$",
+        "title": "CAZZO FUNZIONA EC",
+        "actual_consumption": 1000.00,
+        "uom": "Watt",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_energy.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.20,
+        "currency": "$",
+        "title": "Energy Production",
+        "actual_consumption": 2000.00,
+        "uom": "Watt",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_solar_energy.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.40,
+        "currency": "$",
+        "title": "Water Consumption",
+        "actual_consumption": 3000.00,
+        "uom": "m3",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_water.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.60,
+        "currency": "$",
+        "title": "Gas Consumption",
+        "actual_consumption": 4000.00,
+        "uom": "smc",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_gas.png",
+    }
+]
+
+details_ep = [
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.00,
+        "currency": "$",
+        "title": "CAZZO FUNZIONA EP",
+        "actual_consumption": 1000.00,
+        "uom": "Watt",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_energy.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.20,
+        "currency": "$",
+        "title": "Energy Production",
+        "actual_consumption": 2000.00,
+        "uom": "Watt",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_solar_energy.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.40,
+        "currency": "$",
+        "title": "Water Consumption",
+        "actual_consumption": 3000.00,
+        "uom": "m3",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_water.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.60,
+        "currency": "$",
+        "title": "Gas Consumption",
+        "actual_consumption": 4000.00,
+        "uom": "smc",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_gas.png",
+    }
+]
+
+details_wc = [
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.00,
+        "currency": "$",
+        "title": "CAZZO FUNZIONA WC",
+        "actual_consumption": 1000.00,
+        "uom": "Watt",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_energy.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.20,
+        "currency": "$",
+        "title": "Energy Production",
+        "actual_consumption": 2000.00,
+        "uom": "Watt",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_solar_energy.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.40,
+        "currency": "$",
+        "title": "Water Consumption",
+        "actual_consumption": 3000.00,
+        "uom": "m3",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_water.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.60,
+        "currency": "$",
+        "title": "Gas Consumption",
+        "actual_consumption": 4000.00,
+        "uom": "smc",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_gas.png",
+    }
+]
+
+details_gc = [
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.00,
+        "currency": "$",
+        "title": "CAZZO FUNZIONA GC",
+        "actual_consumption": 1000.00,
+        "uom": "Watt",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_energy.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.20,
+        "currency": "$",
+        "title": "Energy Production",
+        "actual_consumption": 2000.00,
+        "uom": "Watt",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_solar_energy.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.40,
+        "currency": "$",
+        "title": "Water Consumption",
+        "actual_consumption": 3000.00,
+        "uom": "m3",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_water.png",
+    },
+    {
+        "user_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "hw_id": "8cbab44a-5ab0-4eef-8c7d-16ca7398f8ac",
+        "price": 3.60,
+        "currency": "$",
+        "title": "Gas Consumption",
+        "actual_consumption": 4000.00,
+        "uom": "smc",
+        "icon": "http://flexmeter.polito.it/wp-content/uploads/2016/07/icon_gas.png",
+    }
+]
 
 @app.route('/measurements', methods=['GET'])
 def get_tasks():
@@ -211,6 +382,25 @@ def get_aggregate():
 def get_details():
     return jsonify({'details': details})
 
+
+@app.route('/measurements/details/ec', methods=['GET'])
+def get_details_ec():
+    return jsonify({'details_ec': details_ec})
+
+
+@app.route('/measurements/details/ep', methods=['GET'])
+def get_details_ep():
+    return jsonify({'details_ep': details_ep})
+
+
+@app.route('/measurements/details/wc', methods=['GET'])
+def get_details_wc():
+    return jsonify({'details_wc': details_wc})
+
+
+@app.route('/measurements/details/gc', methods=['GET'])
+def get_details_gc():
+    return jsonify({'details_gc': details_gc})
 
 if __name__ == '__main__':
     app.run(host='130.192.163.22', debug=True,
